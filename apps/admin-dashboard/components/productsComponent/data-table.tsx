1eba625bec788d12b57ae8ab5d7c843108ca62ac
Amitav Panda <pandaamitav01@gmail.com>
2024-05-13 04:12:48 -0700

    all the pages for admin panel is completed and fix the build the error

--- apps/admin-dashboard/components/productsComponent/data-table.tsx
+++ apps/admin-dashboard/components/productsComponent/data-table.tsx
@@ -27,7 +27,7 @@ import { useState } from "react";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
-  data: TData[]
+  data: any
 }
 
 export function DataTable<TData, TValue>({
